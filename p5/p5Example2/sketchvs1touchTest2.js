2113a0b934add38ad27781d69e22531f79a2c6f7
Derek R. Dallas <derek.dallas@gmail.com>
2016-08-02 05:20:22 -0500

    added touchIsDown test

---
+++ p5/p5Example2/sketchvs1touchTest2.js
@@ -0,0 +1,34 @@
+// touch test 
+/* this sketch is to test if touching one half of the device screen will be a good way to control the movement of an object. The left half of the screen turns the left half red touching the right half turns the right half blue This feature will be used to move an object back and forth across the screen*/
+
+/*
+The issue with this is that after being touched the screen remains blue.. need to have a boolean test for touch if touching change the screen if not then blank it.
+*/
+
+
+function setup() {
+	createCanvas(500, 500);
+	var hotPink = color(255,138,172);
+	var blueLake = color(139,186,230);
+	var c = color(255, 204, 0); 
+}
+
+function draw() {
+	background(255);	
+	
+	if(touchIsDown && touchX > width/2 && touchY > height-height*.25){
+		noStroke();
+		fill('#3399ff')//blue
+		rect(width/2,0,width/2,height);	
+	}
+	
+	
+	if(touchIsDown && touchX<width/2 && touchY > height-height*.25){
+	// draw left half of screen
+	noStroke();
+	fill('#ff3399');//pink
+	rect(0,0,width/2, height);
+	}
+	
+ 
+}
