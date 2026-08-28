70aaadd88f6a8743b1e72d2f02e79ec9b31ec0d0
Derek R. Dallas <derek.dallas@gmail.com>
2026-08-28 16:14:50 -0500

    Added the dm recruiting webpages

---
+++ dmRecruitingSite/scripts.js
@@ -0,0 +1,35 @@
+// JavaScript Document
+
+console.log("DM recruitment script locked and loaded");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* just test java script
+
+document.querySelector("p#test").innerText = "Melvin Likes Rush!";
+
+
+const paragraph = document.getElementById("test");
+paragraph.style.color = "yellow"; 
+
+
+// Select all elements with the class name 'my-text-class'
+const elements = document.querySelectorAll('.blue');
+
+// Loop through each element and change its color to blue
+elements.forEach(element => {
+  element.style.color = 'yellow'; 
+});
+
+*/
